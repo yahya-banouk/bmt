7ab5d265f44a8025d6706f43aaa80ad195fa62af
YAHYA BANOUK <83559441+yahya-banouk@users.noreply.github.com>
2022-08-22 03:02:02 +0200

    architecture

---
+++ bmt_backend/src/main/java/com/bmt_backend/BmtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bmt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmtBackendApplication.class, args);
+    }
+
+}
---
+++ bmt_backend/src/test/java/com/bmt_backend/BmtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
