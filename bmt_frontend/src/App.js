5e4b781d5689766d4c4278a0f6a2a2bd1b79f8c5
YAHYA BANOUK <83559441+yahya-banouk@users.noreply.github.com>
2022-08-26 19:08:19 +0200

    fixing .git prob

---
+++ bmt_frontend/src/App.js
@@ -0,0 +1,23 @@
+import About from './components/About';
+import AllInOne from './components/AllInOne';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar'
+import Pricing from './components/Pricing';
+import Support from './components/Support'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
